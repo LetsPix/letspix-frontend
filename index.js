917a0e5589d3fbca75e09fae0af8638fe4788f6e
Brian <bfeddes@hotmail.com>
2023-03-30 00:13:19 -0500

    Implemented search bar functionality

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /**
  * Link to actual server
- * https://letspix.azurewebsites.net
+ * https://letspix.azurewebsites.net/
  * Link to client
  * https://polite-plant-0fd6d3810.2.azurestaticapps.net
  * Link to localhost for testing 
  * http://localhost:3000
  */
 
-const url = 'https://letspix.azurewebsites.net';
+const url = 'http://localhost:3000';
+
+// Sending the data to the server to create a new media
 async function createNewMedia() {
     var getDate = new Date();
     var mediaType = document.getElementById("mediaType").value;
@@ -27,8 +29,8 @@ async function createNewMedia() {
     var newMedia = {
         type: mediaType,
         title: title,
-        cast: director,
-        director: cast,
+        cast: cast,
+        director: director,
         country: country,
         date_added: getDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'}), // this will just get the current tim
         release_year: releaseYear,
@@ -60,13 +62,34 @@ async function createNewMedia() {
     }
 }
 
-// function to get data
+// function to read the data from the /all/Netflix endpoint (this grabs all shows and movies on Netflix)
 async function getData() {
     try {
-        const response = await fetch(url + '/all/Netflix', {
+        const response = await fetch(url + '/api/all', {
             method: 'GET',
         });
         const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// function to read data pertaining to a specific film. Filters by title.
+async function getMovieTitle() {
+    const mediaTitle = document.getElementById("searchBar").value;
+    try {
+        const response = await fetch(url + '/api/findtitle', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: mediaTitle })
+        });
+        const data = await response.json();
+        document.getElementById("searchType").innerHTML = data.type;
+        document.getElementById("searchTitle").innerHTML = data.title;
+        document.getElementById("searchCast").innerHTML = data.cast;
         console.log(data);
     } catch (error) {
         console.error(error);
